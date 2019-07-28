c778356d1fc4e352bdc43105b85f633c262adec6
Oleg <salahovol@ya.ru>
2019-07-28 17:19:41 +0600

    combine reducers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+import './App.css'
+
+class App extends Component {
+  render() {
+    const { name, surname, age } = this.props.user
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Топ фото</h1>
+        </header>
+        <p className="App-intro">Здесь будут мои самые залайканые фото</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name} {surname} {age}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = store => {
+  console.log(store)
+  return {
+    user: store.user,
+  }
+}
+
+export default connect(mapStateToProps)(App)
